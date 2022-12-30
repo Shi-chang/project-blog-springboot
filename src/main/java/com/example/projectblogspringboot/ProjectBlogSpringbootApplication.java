f8fd019b39f2b038b097d21164f79c3b4a50bae4
Shi-chang <shichangye@outlook.com>
2022-12-29 17:17:00 -0800

    Initialization of project

---
+++ src/main/java/com/example/projectblogspringboot/ProjectBlogSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectblogspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlogSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBlogSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectblogspringboot/ProjectBlogSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectblogspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlogSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
