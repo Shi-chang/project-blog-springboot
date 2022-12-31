00bca5aedbb1257b0ae8317504afe7be6f00a367
Shi-chang <shichangye@outlook.com>
2022-12-31 15:59:43 -0800

    Add comment controller

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -1,2 +1,37 @@
-package com.springboot.blog.controller;public class CommentController {
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.CommentDto;
+import com.springboot.blog.service.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable Long postId,
+                                                    @RequestBody CommentDto commentDto) {
+        return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<List<CommentDto>> getCommentsByPostId(@PathVariable Long postId) {
+        return new ResponseEntity<>(commentService.getCommentsByPostId(postId), HttpStatus.OK);
+    }
+
+    @GetMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") Long postId,
+                                                     @PathVariable(value = "id") Long commentId) {
+        return new ResponseEntity<>(commentService.getCommentById(postId, commentId), HttpStatus.OK);
+    }
 }
