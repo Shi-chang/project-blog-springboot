b351ae127fe3dc90d42de6af1bdb98c9a844819d
Shi-chang <shichangye@outlook.com>
2022-12-31 16:05:56 -0800

    Add Post repository

--- src/main/java/com/springboot/blog/repository/PostRepository.java
+++ src/main/java/com/springboot/blog/repository/PostRepository.java
@@ -1,2 +1,7 @@
-package com.springboot.blog.repository;public class PostRepository {
+package com.springboot.blog.repository;
+
+import com.springboot.blog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
