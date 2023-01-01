b783b72fafbd9ed2bf7547029ed353275a5ac995
Shi-chang <shichangye@outlook.com>
2023-01-01 11:07:42 -0800

    Add update and delete comment methods

--- src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -8,6 +8,8 @@ import com.springboot.blog.payload.CommentDto;
 import com.springboot.blog.repository.CommentRepository;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.CommentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.Banner;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -19,16 +21,18 @@ public class CommentServiceImpl implements CommentService {
 
     private CommentRepository commentRepository;
     private PostRepository postRepository;
+    private ModelMapper modelMapper;
 
-    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository) {
+    public CommentServiceImpl(CommentRepository commentRepository, PostRepository postRepository, ModelMapper modelMapper) {
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
+        this.modelMapper = modelMapper;
     }
 
     /**
      * Creates a comment.
      *
-     * @param postId the post id that this comment belongs to
+     * @param postId     the post id that this comment belongs to
      * @param commentDto the comment dto
      * @return the comment dto
      */
@@ -61,7 +65,7 @@ public class CommentServiceImpl implements CommentService {
     /**
      * Gets a comment by its id.
      *
-     * @param postId the post id that this comment belongs to
+     * @param postId    the post id that this comment belongs to
      * @param commentId the id of the comment
      * @return the comment dto
      */
@@ -79,19 +83,58 @@ public class CommentServiceImpl implements CommentService {
     }
 
     /**
+     * Updates a comment.
+     *
+     * @param postId     the post id this comment belongs to
+     * @param commentId  the id of the comment
+     * @param commentDto the comment dto
+     * @return the dto of the updated comment
+     */
+    @Override
+    public CommentDto updateComment(Long postId, Long commentId, CommentDto commentDto) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("post", "id", postId));
+
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("Comment", "id", commentId));
+
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogApiException("Comment does not belong to the post", HttpStatus.BAD_REQUEST);
+        }
+
+        comment.setName(commentDto.getName());
+        comment.setBody(commentDto.getBody());
+        comment.setEmail(commentDto.getEmail());
+
+        Comment updatedComment = commentRepository.save(comment);
+
+        return mapToDto(updatedComment);
+    }
+
+    public void deleteComment(Long postId, Long commentId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("post", "id", postId));
+
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("Comment", "id", commentId));
+
+        if (!comment.getPost().getId().equals(post.getId())) {
+            throw new BlogApiException("Comment does not belong to the post", HttpStatus.BAD_REQUEST);
+        }
+
+        commentRepository.delete(comment);
+    }
+
+    /**
      * A helper function that converts a comment entity to a comment dto.
      *
      * @param comment the comment to be converted
      * @return the comment dto
      */
     private CommentDto mapToDto(Comment comment) {
-        CommentDto commentDto = new CommentDto();
+//        CommentDto commentDto = new CommentDto();
+//        commentDto.setId(comment.getId());
+//        commentDto.setBody(comment.getBody());
+//        commentDto.setName(comment.getName());
+//        commentDto.setEmail(comment.getEmail());
 
-        commentDto.setId(comment.getId());
-        commentDto.setBody(comment.getBody());
-        commentDto.setName(comment.getName());
-        commentDto.setEmail(comment.getEmail());
-
+        CommentDto commentDto = modelMapper.map(comment, CommentDto.class);
         return commentDto;
     }
 
@@ -102,12 +145,12 @@ public class CommentServiceImpl implements CommentService {
      * @return the comment
      */
     private Comment mapToEntity(CommentDto commentDto) {
-        Comment comment = new Comment();
+//        Comment comment = new Comment();
+//        comment.setBody(commentDto.getBody());
+//        comment.setName(commentDto.getName());
+//        comment.setEmail(commentDto.getEmail());
 
-        comment.setBody(commentDto.getBody());
-        comment.setName(commentDto.getName());
-        comment.setEmail(commentDto.getEmail());
-
+        Comment comment = modelMapper.map(commentDto, Comment.class);
         return comment;
     }
 }
