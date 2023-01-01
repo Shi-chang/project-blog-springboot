d54e5e2fb2f882045c23dd1e53f582d286780e52
Shi-chang <shichangye@outlook.com>
2022-12-31 16:02:52 -0800

    Add CommentDto

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,2 +1,11 @@
-package com.springboot.blog.payload;public class CommentDto {
+package com.springboot.blog.payload;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String body;
 }
