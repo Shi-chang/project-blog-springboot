d2a15bf188b91b57366fad91f20d38f1e273b4ab
Shi-chang <shichangye@outlook.com>
2023-01-01 11:06:34 -0800

    Change annotation @Data to @Getter and @Setter to avoid infinite loop caused by @ToString among Post and Comment entities during dto mapping

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,11 +1,17 @@
 package com.springboot.blog.payload;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import java.util.Set;
+
+@Getter
+@Setter
 public class PostDto {
     private long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
