5226b4bf7d6bdb7e05678e4fe526bc372cba6c57
Shi-chang <shichangye@outlook.com>
2022-12-31 16:04:56 -0800

    Add PostResponse

--- src/main/java/com/springboot/blog/payload/PostResponse.java
+++ src/main/java/com/springboot/blog/payload/PostResponse.java
@@ -1,2 +1,22 @@
-package com.springboot.blog.payload;public class PostResponse {
+package com.springboot.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * A response to the client side for the post resource.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostResponse {
+    private List<PostDto> content;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
 }
