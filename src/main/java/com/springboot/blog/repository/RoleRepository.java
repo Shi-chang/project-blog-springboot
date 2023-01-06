6c2faa2e05f20b2fae313e68cf693a1d474f6072
Shi-chang <shichangye@outlook.com>
2023-01-06 14:33:25 -0800

    Add RoleRepository

--- src/main/java/com/springboot/blog/repository/RoleRepository.java
+++ src/main/java/com/springboot/blog/repository/RoleRepository.java
@@ -1,2 +1,10 @@
-package com.springboot.blog.repository;public interface RoleRepository {
+package com.springboot.blog.repository;
+
+import com.springboot.blog.entity.Role;
+
+import java.util.Optional;
+
+public interface RoleRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
 }
