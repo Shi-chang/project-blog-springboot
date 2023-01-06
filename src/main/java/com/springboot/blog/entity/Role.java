c55bec7001b6904b600edca3407132fc034ec634
Shi-chang <shichangye@outlook.com>
2023-01-06 14:29:59 -0800

    Add Role entity

--- src/main/java/com/springboot/blog/entity/Role.java
+++ src/main/java/com/springboot/blog/entity/Role.java
@@ -1,2 +1,23 @@
-package com.springboot.blog.entity;public class Role {
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
 }
