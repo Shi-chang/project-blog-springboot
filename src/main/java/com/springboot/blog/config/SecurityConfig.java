e03fa2922470f26bfb2710b74b0ba70c9c45c1a4
Shi-chang <shichangye@outlook.com>
2023-01-06 14:28:20 -0800

    Add SerucityConfig

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,2 +1,65 @@
-package com.springboot.blog.config;public class SecurityConfig {
+package com.springboot.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf()
+                .disable()
+                .authorizeHttpRequests(authorize ->
+                        //authorize.anyRequest().authenticated()
+                        authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails alan = User.builder()
+//                .username("alan")
+//                .password(passwordEncoder().encode("123456"))
+//                .roles("ADMIN").build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password(passwordEncoder().encode("123456"))
+//                .roles("USER").build();
+//
+//        return new InMemoryUserDetailsManager(alan, susan);
+//    }
 }
