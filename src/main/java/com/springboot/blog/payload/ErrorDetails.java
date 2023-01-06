d98d324290cb1f5ba325cd0b37baffa84e864a79
Shi-chang <shichangye@outlook.com>
2023-01-06 14:32:35 -0800

    Add @NotEmpty, @Size and @Size annotation constraints

--- src/main/java/com/springboot/blog/payload/ErrorDetails.java
+++ src/main/java/com/springboot/blog/payload/ErrorDetails.java
@@ -1,2 +1,18 @@
-package com.springboot.blog.payload;public class ErrorDetails {
+package com.springboot.blog.payload;
+
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
 }
--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.payload;
 
-import lombok.Data;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +11,16 @@ import java.util.Set;
 @Setter
 public class PostDto {
     private long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters.")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters.")
     private String description;
+
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
