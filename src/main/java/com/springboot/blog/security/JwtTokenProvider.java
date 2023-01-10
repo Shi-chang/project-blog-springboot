b1c74bbdfdd7d5c0b026e3a7ef19554f0bf6d253
Shi-chang <shichangye@outlook.com>
2023-01-10 11:11:47 -0800

    Add JwtTokenProvider

--- src/main/java/com/springboot/blog/security/JwtTokenProvider.java
+++ src/main/java/com/springboot/blog/security/JwtTokenProvider.java
@@ -1,2 +1,91 @@
-package com.springboot.blog.security;public class JwtTokenProvider {
+package com.springboot.blog.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@PropertySource("classpath:application.properties")
+public class JwtTokenProvider {
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration.milliseconds}")
+    private long jwtExpirationDate;
+
+    /**
+     * Generates a jwt token.
+     *
+     * @param authentication the authentication
+     * @return a jwt token
+     */
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+
+        Date currentDate = new Date();
+
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+
+        String token = Jwts
+                .builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+
+        return token;
+    }
+
+    /**
+     * Gets the secret key.
+     *
+     * @return the secret key
+     */
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    /**
+     * Gets the username from the token provided.
+     *
+     * @param token the token provided
+     * @return the username
+     */
+    public String getUsername(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJws(token).getBody();
+
+        String username = claims.getSubject();
+
+        return username;
+    }
+
+    /**
+     * Validates jwt token.
+     *
+     * @param token the token
+     * @return true if the token is valid, false otherwise
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException(e);
+        } catch (MalformedJwtException e) {
+            throw new RuntimeException(e);
+        } catch (SignatureException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
