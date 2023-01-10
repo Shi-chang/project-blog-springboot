a423a5a6e04ab9f3875539b0b9c2311675509d71
Shi-chang <shichangye@outlook.com>
2023-01-09 21:41:17 -0800

    Add CategoryController

--- src/main/java/com/springboot/blog/controller/CategoryController.java
+++ src/main/java/com/springboot/blog/controller/CategoryController.java
@@ -1,2 +1,29 @@
-package com.springboot.blog.controller;public class CategoryController {
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.CategoryDto;
+import com.springboot.blog.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto) {
+        CategoryDto savedCategory = categoryService.addCategory(categoryDto);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
 }
