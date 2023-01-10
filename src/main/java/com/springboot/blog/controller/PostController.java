852ef9ae4ca12defb9dce0b758435ea50ef96655
Shi-chang <shichangye@outlook.com>
2023-01-09 21:41:40 -0800

    Add PostController

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
