a517978474aacf23d3e5b64cb2a5fb7c4209243f
Shi-chang <shichangye@outlook.com>
2023-01-10 11:13:22 -0800

    Add AuthServiceImple

--- src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/AuthServiceImpl.java
@@ -1,2 +1,82 @@
-package com.springboot.blog.service.impl;public class AuthServiceImpl {
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.User;
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.exception.BlogApiException;
+import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
+import com.springboot.blog.repository.RoleRepository;
+import com.springboot.blog.repository.UserRepository;
+import com.springboot.blog.security.JwtTokenProvider;
+import com.springboot.blog.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.HashSet;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+    private JwtTokenProvider jwtTokenProvider;
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager,
+                           UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder,
+                           JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public String login(LoginDto loginDto) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtTokenProvider.generateToken(authentication);
+
+        return token;
+    }
+
+    @Override
+    public String register(RegisterDto registerDto) {
+        // Check if the username is already registered
+        if (userRepository.existsByUsername(registerDto.getUsername())) {
+            throw new BlogApiException("This username already exists.", HttpStatus.BAD_REQUEST);
+        }
+
+        // Check if the email is already registered
+        if (userRepository.existsByEmail(registerDto.getEmail())) {
+            throw new BlogApiException("This email already exists.", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setName(registerDto.getName());
+        user.setUsername(registerDto.getUsername());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = (Role) roleRepository.findByName("ROLE_USER").get();
+        roles.add(userRole);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+
+        return "User register successfully.";
+    }
 }
